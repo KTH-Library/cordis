36c64272b2f957fbe31fa2a22f6cec246d34cdcd
Markus Skyttner <markussk@kth.se>
2023-02-10 10:18:30 +0100

    update data prep script

--- data-raw/cordis_data.R
+++ data-raw/cordis_data.R
@@ -16,12 +16,12 @@ dl <- function(url, destfile, ...)
 # -------------
 # cordisref-data
 
-# retrieve dl data from RSS
+# retrieve dl data from RSS for cordis ref-data
 # NB: these files have been removed from cordisref
 # organizationActivityType
 # sicCode
 
-rss <-
+rss_ref <-
   "https://data.europa.eu/api/hub/search/en/feeds/datasets/cordisref-data.rss" |>
   GET() |> content(as = "text", encoding = "UTF-8") |>
   strsplit(split = "\n") |> unlist() |> enframe(name = NULL, value = "rss") |>
@@ -30,40 +30,49 @@ rss <-
   mutate(filename = gsub(x = rss, ".*?(cordisref-.*?\\..{3})[\"]*.*$", "\\1", perl = TRUE)) |>
   mutate(is_xls = grepl("xls|xlsx", filename)) |>
   mutate(is_csv = grepl("csv", filename)) |>
+  mutate(is_zip = grepl("zip", filename)) |>
+  mutate(is_txt = grepl("txt", filename)) |>
   mutate(tbl = gsub(x = filename, "\\..{3}|-csv|-xlsx|cordisref-", "")) |>
-  select(tbl, is_xls, is_csv, filename, url) |>
+  select(tbl, starts_with("is"), filename, url) |>
   mutate(destfile = file.path(cachedir, filename)) |>
   arrange(desc(tbl))
 
+# rss_ref$tbl |> unique() |> paste(collapse = "\n") |> cat()
+
 # download xlsx files (NB: xls is misleading)
 files_xl <-
-  rss |> filter(is_xls, tbl == "h2020topicKeywords") |>
+  rss_ref |> filter(is_xls, tbl == "h2020topicKeywords") |>
   pmap(dl) |> unlist() |>
   read_xlsx()
 
 # download txt files
 files_txt <-
-  rss |> filter(grepl("txt", destfile)) |>
+  rss_ref |> filter(grepl("txt", destfile)) |>
   pmap(function(destfile, ...) read_lines(destfile)) |> unlist() |>
   strsplit(split = ";") |> enframe(name = "id", value = "cat") |>
   unnest_wider(cat, names_sep = "_")
 
 # download CSV files
-rss |> filter(is_csv) |> pmap_chr(dl)
+rss_ref |> filter(is_csv) |> pmap_chr(dl)
+
+# NB: all zip files here contain only one CSV, no need to unpack these individually
+# files_csvz <- rss_ref |> filter(is_zip, is_csv) |> pmap(lsz)
 
 # read CSV files
 files_csv <-
-  rss |> filter(is_csv) |>
+  rss_ref |> filter(is_csv) |>
   pmap(function(destfile, ...) readr::read_csv2(destfile, guess_max = 5e4, show_col_types = F))
+
 
 # collect all data (CSV, excel, txt)
 tbls_csv <-
-  rss |> filter(is_csv) |>
+  rss_ref |> filter(is_csv) |>
+  mutate(tbl = paste0("ref_", tbl)) |>
   pull("tbl") |> tolower()
 
-data <- setNames(files_csv, tbls_csv)
-data$h2020topicKeywords <- files_xl
-data$fp7subprogrammes <- fp7subs
+data_ref <- setNames(files_csv, tbls_csv)
+data_ref$ref_h2020topicKeywords <- files_xl
+data_ref$ref_fp7subprogrammes <- files_txt
 
 #--------
 #
@@ -129,12 +138,14 @@ rss_h2020 <-
   strsplit(split = "\n") |> unlist() |> enframe(name = NULL, value = "rss") |>
   filter(grepl("csv|txt", rss)) |>
   mutate(url = gsub(x = rss, ".*?(http.*?)\".*", "\\1")) |>
-  mutate(filename = gsub(x = rss, ".*?(cordis-h2020.*?\\..{3}).*$", "\\1")) |>
-  mutate(filename = gsub(x = filename, ".*?data/(.*?\\..{3}).*$", "\\1")) |>
-  mutate(tbl = gsub(x = filename, "\\..{3}|-csv|cordis-", "")) |>
-  select(tbl, filename, url) |>
-  mutate(destfile = file.path(cachedir, filename)) |>
-  mutate(is_zip = grepl("\\.zip$", destfile))
+  mutate(filename = gsub(x = url, ".*?([^/]*?\\..{3})$", "\\1", perl = TRUE)) |>
+  mutate(is_zip = grepl("\\.zip", filename)) |>
+  mutate(is_csv = grepl("\\.csv", filename)) |>
+  # mutate(filename = gsub(x = rss, ".*?(cordis-h2020.*?\\..{3}).*$", "\\1")) |>
+  # mutate(filename = gsub(x = filename, ".*?data/(.*?\\..{3}).*$", "\\1")) |>
+  mutate(tbl = paste0("h2020_", gsub(x = filename, "\\..{3}|-csv|cordis-|_*h2020", ""))) |>
+  select(tbl, starts_with("is"), filename, url) |>
+  mutate(destfile = file.path(cachedir, filename))
 
 # download these datasets locally and read the data
 rss_h2020 |> pmap_chr(dl)
@@ -153,7 +164,7 @@ z_h2020 <-
   csvz_h2020 |> pmap(read_zip) |>
   setNames(nm = csvz_h2020$tbl_z)
 
-#data_h2020 |> map(readr::problems)
+#z_h2020 |> map(readr::problems)
 
 iprs <-
   rss_h2020 |> filter(!is_zip) |>
@@ -168,23 +179,85 @@ iprs <-
 
 pis <- file.path(cachedir, "pi.xlsx") |> readxl::read_excel()
 
-"http://digital-agenda-data.eu/download/digital-agenda-scoreboard-key-indicators.csv.zip" %>%
-  download.file(file.path(cachedir, "scoreboard.csv.zip"))
+fp_scoreboard <- file.path(cachedir, "scoreboard.csv.zip")
 
-scoreboard <- readr::read_csv(
-  unz(ft2, filename = unzip(file.path(cachedir, "scoreboard.csv.zip"), list = TRUE)$Name),
-  col_types = "ccccccccc") |>
-  as_tibble()
+"http://digital-agenda-data.eu/download/digital-agenda-scoreboard-key-indicators.csv.zip" |>
+  download.file(fp_scoreboard)
+
+scoreboard <-
+  readr::read_csv(fp_scoreboard, col_types = "ccccccccc") |> as_tibble()
 
 data_h2020 <- c(z_h2020, iprs, list(h2020_pi = pis), list(h2020_scoreboard = scoreboard))
+
+#names(data_h2020)
+
+
+
+# -------------
+# CORDIS FP 7 projects data
+#
+rss_fp7 <-
+  "https://data.europa.eu/api/hub/search/en/feeds/datasets/cordisfp7projects.rss" |>
+  GET() |> content(as = "text", encoding = "UTF-8") |>
+  strsplit(split = "\n") |> unlist() |> enframe(name = NULL, value = "rss") |>
+  filter(grepl("csv|txt", rss)) |>
+  mutate(url = gsub(x = rss, ".*?(http.*?)\".*", "\\1")) |>
+  mutate(filename = gsub(x = url, ".*?([^/]*?\\..{3})$", "\\1", perl = TRUE)) |>
+  mutate(is_zip = grepl("zip", filename)) |>
+  mutate(is_csv = grepl("csv", filename)) |>
+  mutate(tbl = paste0("fp7_", tolower(gsub(x = filename, "\\..{3}|-csv|cordis-|FP7PC_|_fp7|fp7", "")))) |>
+  select(tbl, is_zip, is_csv, filename, url) |>
+  mutate(destfile = file.path(cachedir, filename)) |>
+  arrange(desc(tbl))
+
+# download the FP7 files
+rss_fp7 |> pmap_chr(dl)
+
+# unpack the zip files first
+csvz_fp7 <-
+  rss_fp7 |> filter(is_zip) |> pmap(lsz) |>
+  setNames(nm = grep("\\.zip$", rss_fp7$destfile, value = TRUE)) |>
+  stack() |> as_tibble() |>
+  rename(target = "values", destfile = "ind") |>
+  left_join(rss_fp7 |> filter(grepl("\\.zip$", destfile)), by = "destfile") |>
+  mutate(tbl_z = paste0("fp7_", gsub("csv/|\\.csv", "", target)))  |>
+  select(tbl_z, everything())
+
+z_fp7 <-
+  csvz_fp7 |> pmap(read_zip) |>
+  setNames(nm = csvz_fp7$tbl_z)
+
+#z_fp7 |> map(readr::problems)
+#
+# z_fp7$fp7_organization
+# # A tibble: 2 × 5
+#     row   col expected actual  file
+#   <int> <int> <chr>    <chr>   <chr>
+# 1 13598    18 a double xxxxxxx ""
+# 2 13598    21 a number xxxxx   ""
+
+csv_fp7 <-
+  rss_fp7 |>
+  filter(!is_zip) |>
+  mutate(sep = ifelse(tbl == "fp7_projectirps", ",", ";")) |>
+  pmap(function(destfile, sep, ...) readr::read_delim(destfile, delim = sep, guess_max = 5e4, show_col_types = F)) |>
+  setNames(nm = rss_fp7 |> filter(!is_zip) |> getElement("tbl"))
+
+# data_fp7 |> map(readr::problems)
+
+data_fp7 <- c(csv_fp7, z_fp7)
+
+
+
 
 #------------
 # Write to database
 #
 
 # combine previous batches of data
-d <- c(data, data_he, data_h2020)
+d <- c(data_ref, data_he, data_h2020, data_fp7)
 
+# reencode all strings for "invalid bytecode sequences"
 res <-
   d |>
   map(function(x) x |> mutate(across(
@@ -219,7 +292,9 @@ dump_parquet <- function(dir = "temp") {
   dbpath <- normalizePath(file.path(cachedir, "cordisdb"))
   con <- dbConnect(duckdb(dbdir = dbpath))
   on.exit(dbDisconnect(con, shutdown = TRUE))
-  stopifnot(dir.exists(file.path(cachedir, dir)))
+
+  if (!dir.exists(file.path(cachedir, dir)))
+    dir.create(file.path(cachedir, dir), recursive = TRUE)
 
   tbls <- con |> dbListTables()
 
@@ -232,7 +307,8 @@ dump_parquet <- function(dir = "temp") {
   parquets |>
     enframe(name = "table", value = "rows") |>
     unnest(rows) |>
-    arrange(desc(rows))
+    arrange(desc(rows)) |>
+    print(n = 50)
 }
 
 # dump all the data into .parquet files
@@ -256,7 +332,7 @@ uploadz <- dir(dumpdir, pattern = ".parquet")
 # list released datasets
 "KTH-Library/cordis-data" |> pb_releases()
 
-# create new tag for new data
+# NB: ONLY IF NEEDED, create new tag for new data
 "KTH-Library/cordis-data" |> pb_new_release(tag = "v0.2.0")
 
 # TODO: fix this, see , do it manually for now...
